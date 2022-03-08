c025aa627976a0a9b7697a5b938b2014cd5f5d24
Duncan Jones <dj226@outlook.com>
2022-03-08 17:17:07 +0000

    Started on the css layout for the mapping page

--- static/javascript/friendsCurrentLevelTest.js
+++ static/javascript/friendsCurrentLevelTest.js
@@ -1,12 +1,11 @@
-// $(document).ready(function(){
-//     alert("Working");
-// });
+$(document).ready(() => alert("Working"));
+
 
 function loadDoc() {
     const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
+    xhttp.onreadystatechange = () => {
         // console.log(this.responseText);
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             document.getElementById("test").innerHTML = this.responseText;
         }
     };
