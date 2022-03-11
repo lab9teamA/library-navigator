bbb8b82f20c83f2f8c021f8ea44e77b9d07ed1db
Civan Dogan <ciwoerim@gmail.com>
2022-03-11 11:53:49 +0000

    Models created. Passfield to be decided. Tests needed.

--- libnav/models.py
+++ libnav/models.py
@@ -1,1 +1,34 @@
 # Create your models here.
+from django.db import models
+
+
+class Subject(models.Model):
+    id = models.AutoField(primary_key=True , unique=True)
+    name = models.CharField(max_length=50)
+
+
+class Location(models.Model):
+    floor = models.IntegerField()
+    bookCaseID = models.CharField(max_length=10)
+
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    coverImage = models.ImageField()
+    checkedOut = models.IntegerField()
+    checkedOut = models.DateField()
+    numberOfPage = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    likes = models.IntegerField()
+    location = models.ForeignKey(Location , on_delete=models.PROTECT)
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30)
+    # TODO password = 1
+    picture = models.ImageField()
+    isReading = models.ManyToManyField(Book,related_name="user_isReading")
+    recommends = models.ManyToManyField(Book,related_name="user_recommends")
