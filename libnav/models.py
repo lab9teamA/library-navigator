4f82bc16b74f6cc3dc56fe1193c41eee99058b85
Civan Dogan <ciwoerim@gmail.com>
2022-03-11 14:52:11 +0000

    User model implemented by django default user model.

--- libnav/models.py
+++ libnav/models.py
@@ -1,15 +1,23 @@
 # Create your models here.
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Subject(models.Model):
     id = models.AutoField(primary_key=True , unique=True)
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Location(models.Model):
     floor = models.IntegerField()
     bookCaseID = models.CharField(max_length=10)
+
+    def __str__(self):
+        return  str(self.bookCaseID) +" at "+str(self.floor)
+
+
 
 
 class Book(models.Model):
@@ -17,18 +25,22 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     coverImage = models.ImageField()
-    checkedOut = models.IntegerField()
-    checkedOut = models.DateField()
+    checkedOut = models.DateTimeField()
+    publishDate = models.DateField()
     numberOfPage = models.IntegerField()
     description = models.CharField(max_length=1000)
     likes = models.IntegerField()
     location = models.ForeignKey(Location , on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.title
+
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=30)
-    # TODO password = 1
-    picture = models.ImageField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to="profile_images",blank=True)
     isReading = models.ManyToManyField(Book,related_name="user_isReading")
     recommends = models.ManyToManyField(Book,related_name="user_recommends")
+
+    def __str__(self):
+        return self.user.username
