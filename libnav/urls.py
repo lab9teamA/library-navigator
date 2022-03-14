c1c4c9c239459da497377ab87bdb408edc628584
David Jaramillo <2566008j@student.gla.ac.uk>
2022-03-14 12:25:50 +0000

    added urls and updated templates

--- libnav/urls.py
+++ libnav/urls.py
@@ -6,4 +6,8 @@ app_name = 'libnav'
 urlpatterns = [
     path('', views.home, name='home'),
     path('map/', views.map, name='map'),
+    path('about/', views.about, name= 'about'),
+    path('profile/<user-id>/', views.profile, name = 'profile'),
+    path('book/<ISBN>/', views.book, name ='book'),
+    path('login/', views.user_login, name='login')
 ]
