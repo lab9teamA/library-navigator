f4dd2b7dc419b383e2077ea28515dc03e6de3b58
Duncan Jones <2561101J@student.gla.ac.uk>
2022-03-23 17:47:13 +0000

    Login/Register forums looking lovely (Needs to be tested)

--- libnav/forms.py
+++ libnav/forms.py
@@ -1,15 +1,59 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import TextInput, PasswordInput, EmailInput, URLInput, FileInput
 from libnav.models import User, UserProfile
 
+
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget = forms.PasswordInput())
+
+    password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
+
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'email', 'password')
+        widgets = {'username': TextInput(attrs={
+            'type': 'text',
+            'class': 'input',
+            'id': 'id_username',
+            'placeholder': ' ',
+            }),
+            'email': EmailInput(attrs={
+            'type': 'email',
+            'class': 'input',
+            'id': 'id_email',
+            'placeholder': ' ',
+            }),
+            'password': PasswordInput(attrs={
+            'type': 'password',
+            'class': 'input',
+            'id': 'id_password',
+            'placeholder': ' ',
+            })}
+
+
 
 class UserProfileForm(forms.ModelForm):
+
     class Meta:
         model = UserProfile
         fields = ('picture','website', 'description',)
+        widgets = {
+            'picture': FileInput(attrs={
+            'type': 'file',
+            'class': 'file-input',
+            'id': 'id_picture',
+            'accept': 'image/*',
+            }),
+            'website': URLInput(attrs={
+            'type': 'email',
+            'class': 'input',
+            'id': 'id_email',
+            'placeholder': ' ',
+            }),
+            'description': TextInput(attrs={
+            'type': 'password',
+            'class': 'input',
+            'id': 'id_password',
+            'placeholder': ' ',
+            })}
